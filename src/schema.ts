a85df379ad58976a1a18ac87bf0136fd0202d612
Kirstin <kirstin.petersen@lego.com>
2023-11-27 12:18:54 +0000

    first commit - project setup + Apollo server setup + typeDefs created for schema

---
+++ src/schema.ts
@@ -0,0 +1,25 @@
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!
+        title: String!
+        platform: [String!]!
+    }
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+    }
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+    }
+    type Query {
+        # when the user queries reviews they expect to get the Reviews - entry point - where queryies can start from
+        reviews: [Reviews]
+        games: [Game]
+        authors: [Author]
+    }
+`
+
+//int, float, strings, boolean, ID
