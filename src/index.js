8137df6045a7a4af5531d7295e7eb1bc87c9c149
Kirstin <kirstin.petersen@lego.com>
2023-11-28 16:21:29 +0000

    update schema and index based on interconnected data

--- src/index.js
+++ src/index.js
@@ -10,13 +10,41 @@ const resolvers = {
         games() {
             return db.games
         }, 
+        game(_, args) {
+            return db.games.find((game ) => game.id === args.id)
+        },
         reviews() {
             return db.reviews
         }, 
+        review(_, args) {
+            return db.reviews.find((review ) => review.id === args.id)
+        },
         authors() {
             return db.authors
+        }, 
+        author(_, args) {
+            return db.authors.find((author ) => author.id === args.id)
+        },
+    }, 
+    // Nested query to get all the reviews for that game
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id === parent.id)
         }
-    }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+    }, 
+    Review: {
+        author(parent) {
+            return db.authors.find((author) => author.id === parent.author_id)
+        }, 
+        game(parent) {
+            return db.games.find((game) => game.id === parent.game_id)
+        }
+    }, 
 }
 
 //server setup
--- src/schema.js
+++ src/schema.js
@@ -3,22 +3,29 @@ export const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
     type Query {
         # when the user queries reviews they expect to get the Reviews - entry point - where queryies can start from
         reviews: [Review]
+        review(id: ID!): Review
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID!): Author
     }
 `
 
