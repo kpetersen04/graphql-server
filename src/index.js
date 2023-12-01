4a6ec16cf0a442b546eb45f99913999bbe09a331
Kirstin <kirstin.petersen@lego.com>
2023-12-01 09:37:11 +0000

    add in addGame, deleteGame and updateGame Mutations

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const resolvers = {
             return db.authors.find((author ) => author.id === args.id)
         },
     }, 
-    // Nested query to get all the reviews for that game
+    // Nested query to get all the reviews for that game or the author/game for the review 
     Game: {
         reviews(parent) {
             return db.reviews.filter((r) => r.game_id === parent.id)
@@ -45,6 +45,32 @@ const resolvers = {
             return db.games.find((game) => game.id === parent.game_id)
         }
     }, 
+    Mutation: {
+        deleteGame(_, args) {
+            return db.games = db.games.filter((game) => game.id !== args.id)
+        }
+    }, 
+    Mutation: {
+        addGame(_, args) {
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+            db.games.push(game); 
+            return game
+        }
+    }, 
+    Mutation: {
+        updateGame(_, args) {
+          db.games = db.games.map((game) => {
+            if (game.id === args.id) {
+                return {...game, ...args.edits}
+            }
+            return game
+          })
+          return db.games.find((game) => game.id === args.id)
+        }
+    }
 }
 
 //server setup
